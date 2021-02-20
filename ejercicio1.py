831c71239d22039d6df691ac31f34b013d7ce23e
Elvira-Camarillo <gcamarilloluna@gmail.com>
2021-02-20 12:40:11 -0600

    Delete else in fuction len_number:octocat:

--- ejercicio1.py
+++ ejercicio1.py
@@ -1,19 +1,34 @@
-# Crear una función que reciba un número como argumento y devuelva el largo de este número
+## Ejercicio 1
+​
+# Crear una funcion que reciba un numero como argumento y devuelva el largo de este numero
+# ​
+# **Ejemplo**
+# 
+# `number_length(10) -> 2`
+# ​
+# `number_length(10000) -> 5`
+# ​
+# `number_length(4321) -> 4`
+# ​
+# **Restricciones**
+# - El numero no puede ser negativo
+# - El numero que se MANDA a la funcion tiene que ser de tipo INT
+# - **NO** se puede utilizar el metodo length
 
-# Ejercicio 1
+### Solución del ejercicio
 
 def len_number(number):
     if type(number) != int:
         return 'Ingresa un número por favor'
     elif number < 0 :
         return 'Ingresa números positivos por favor'
-    else:
-        count = 0
-        number_to_string = str(number)
-        for caracter in number_to_string:
-            count += 1
-        return count
+    count = 0
+    number_to_string = str(number)
+    for character in number_to_string:
+        count += 1
+    return count
 
 len_number(5555555)
 len_number('56')
 len_number(-5)
+len_number(True)
